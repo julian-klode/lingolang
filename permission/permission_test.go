88c1e05dc6a3cfb07e227daf7ac0eed5c048664b
Julian Andres Klode <jak@jak-linux.org>
2017-06-20 16:46:47 +0200

    permission: Add permission_test.go
    Some unit tests to bring up coverage here.

---
+++ permission/permission_test.go
@@ -0,0 +1,52 @@
+// (C) 2017 Julian Andres Klode <jak@jak-linux.org>
+// Licensed under the 2-Clause BSD license, see LICENSE for more information.
+
+package permission
+
+import "testing"
+
+var testcasesPermissionString = []struct {
+	perm     BasePermission
+	expected string
+}{
+	// Basic combinations
+	{Owned | Mutable, "om"},
+	{Owned | LinearValue, "ol"},
+	{Owned | Value, "ov"},
+	{Owned | ReadOnly, "or"},
+	{Owned | None, "on"},
+	{Mutable, "m"},
+	{LinearValue, "l"},
+	{Value, "v"},
+	{Any, "a"},
+	{ReadOnly, "r"},
+	{None, "n"},
+	{Write, "w"},
+	// Other cases
+	{Write | ExclWrite, "wW"},
+	{Write | ExclRead, "wR"},
+	{Read | ExclRead, "rR"},
+}
+
+func TestPermissionString(t *testing.T) {
+	for _, testCase := range testcasesPermissionString {
+		testCase := testCase
+		t.Run(testCase.expected, func(t *testing.T) {
+			result := testCase.perm.String()
+			if result != testCase.expected {
+				t.Errorf("Unexpected result %s, expected %s", result, testCase.expected)
+			}
+		})
+	}
+}
+
+func TestIsAPermission(t *testing.T) {
+	Permission.isAPermission(Any)
+	Permission.isAPermission(&PointerPermission{})
+	Permission.isAPermission(&ChanPermission{})
+	Permission.isAPermission(&ArraySlicePermission{})
+	Permission.isAPermission(&MapPermission{})
+	Permission.isAPermission(&StructPermission{})
+	Permission.isAPermission(&FuncPermission{})
+	Permission.isAPermission(&InterfacePermission{})
+}
