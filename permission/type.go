2f9529674206053a8d9d112fba5274728d601357
Julian Andres Klode <jak@jak-linux.org>
2017-06-27 10:15:41 +0200

    permission: Add TypeMapper and TypeMapper.NewFromType()
    The NewFromType() function constructs a permission matching
    a given type. The idea is that this is the maximum set of
    permissions possible, and can be restricted by annotations
    or context with some kind of Permission.LimitTo(Permission)
    operation.
    
    Some things are still missing, for example, how to deal with
    ownership, and contextual permissions (a compat mode vs current
    mode thingy).

---
+++ permission/type.go
@@ -0,0 +1,123 @@
+// (C) 2017 Julian Andres Klode <jak@jak-linux.org>
+// Licensed under the 2-Clause BSD license, see LICENSE for more information.
+//
+
+package permission
+
+import (
+	"go/types"
+)
+
+const basicPermission = Mutable
+
+// TypeMapper maintains a map from types to permissions, so recursive types
+// and type references can be handled correctly.
+type TypeMapper map[types.Type]Permission
+
+// NewTypeMapper constructs a new type mapper
+func NewTypeMapper() TypeMapper {
+	return make(TypeMapper)
+}
+
+// NewFromType constructs a new permission that is as permissive
+// as possible for a given type.
+//
+// TODO ownership
+func (typeMapper TypeMapper) NewFromType(t0 types.Type) (result Permission) {
+	if r, ok := typeMapper[t0]; ok {
+		return r
+	}
+	// Simple type dispatch
+	switch t := t0.(type) {
+	case *types.Array:
+		return typeMapper.newFromArrayType(t)
+	case *types.Slice:
+		return typeMapper.newFromArrayType(t)
+	case *types.Chan:
+		return typeMapper.newFromChanType(t)
+	case *types.Pointer:
+		return typeMapper.newFromPointerType(t)
+	case *types.Map:
+		return typeMapper.newFromMapType(t)
+	case *types.Struct:
+		return typeMapper.newFromStructType(t)
+	case *types.Signature:
+		return typeMapper.newFromSignatureType(t)
+	case *types.Interface:
+		return typeMapper.newFromInterfaceType(t)
+	case *types.Basic:
+		return basicPermission
+	default:
+		// Fall through to the underlying type.
+		t0 = t.Underlying()
+		if t0 != t {
+			return typeMapper.NewFromType(t0)
+		}
+	}
+	return None
+}
+
+func (typeMapper TypeMapper) newFromChanType(t *types.Chan) Permission {
+	perm := &ChanPermission{BasePermission: basicPermission}
+	typeMapper[t] = perm
+	perm.ElementPermission = typeMapper.NewFromType(t.Elem())
+	return perm
+}
+
+type arrayOrSliceType interface {
+	types.Type
+	Elem() types.Type
+}
+
+func (typeMapper TypeMapper) newFromArrayType(t arrayOrSliceType) Permission {
+	perm := &ArraySlicePermission{BasePermission: basicPermission}
+	typeMapper[t] = perm
+	perm.ElementPermission = typeMapper.NewFromType(t.Elem())
+	return perm
+}
+
+func (typeMapper TypeMapper) newFromMapType(t *types.Map) Permission {
+	perm := &MapPermission{BasePermission: basicPermission}
+	typeMapper[t] = perm
+	perm.KeyPermission = typeMapper.NewFromType(t.Key())
+	perm.ValuePermission = typeMapper.NewFromType(t.Elem())
+	return perm
+}
+func (typeMapper TypeMapper) newFromPointerType(t *types.Pointer) Permission {
+	perm := &PointerPermission{BasePermission: basicPermission}
+	typeMapper[t] = perm
+	perm.Target = typeMapper.NewFromType(t.Elem())
+	return perm
+}
+func (typeMapper TypeMapper) newFromStructType(t *types.Struct) Permission {
+	perm := &StructPermission{BasePermission: basicPermission}
+	typeMapper[t] = perm
+	for i := 0; i < t.NumFields(); i++ {
+		perm.Fields = append(perm.Fields, typeMapper.NewFromType(t.Field(i).Type()))
+	}
+	return perm
+}
+
+func (typeMapper TypeMapper) newFromSignatureType(t *types.Signature) Permission {
+	perm := &FuncPermission{BasePermission: basicPermission}
+	typeMapper[t] = perm
+	if r := t.Recv(); r != nil {
+		perm.Receivers = append(perm.Receivers, typeMapper.NewFromType(r.Type()))
+	}
+	for ps, i := t.Params(), 0; i < ps.Len(); i++ {
+		perm.Params = append(perm.Params, typeMapper.NewFromType(ps.At(i).Type()))
+	}
+	for res, i := t.Results(), 0; i < res.Len(); i++ {
+		perm.Results = append(perm.Results, typeMapper.NewFromType(res.At(i).Type()))
+	}
+	return perm
+}
+
+func (typeMapper TypeMapper) newFromInterfaceType(t *types.Interface) Permission {
+	perm := &InterfacePermission{BasePermission: basicPermission}
+	typeMapper[t] = perm
+	for i := 0; i < t.NumMethods(); i++ {
+		perm.Methods = append(perm.Methods, typeMapper.NewFromType(t.Method(i).Type()))
+	}
+	return perm
+}
---
+++ permission/type_test.go
@@ -0,0 +1,133 @@
+// (C) 2017 Julian Andres Klode <jak@jak-linux.org>
+// Licensed under the 2-Clause BSD license, see LICENSE for more information.
+package permission
+
+import (
+	"go/ast"
+	goparser "go/parser"
+	"go/token"
+	"go/types"
+	"reflect"
+	"testing"
+)
+
+// newInterfaceWithMethod creates a recursive interface permission
+func newInterfaceWithMethod() *InterfacePermission {
+	iface := &InterfacePermission{
+		BasePermission: Mutable,
+		Methods: []Permission{
+			&FuncPermission{
+				BasePermission: Mutable,
+				Params:         []Permission{Mutable},
+			},
+		},
+	}
+	iface.Methods[0].(*FuncPermission).Receivers = []Permission{iface}
+	iface.Methods[0].(*FuncPermission).Results = []Permission{iface}
+	return iface
+}
+
+// testCases contains tests for the permission parser.
+var testCases = map[string]Permission{
+	"struct { x int64; y interface {}}": &StructPermission{
+		BasePermission: Mutable,
+		Fields: []Permission{Mutable, &InterfacePermission{
+			BasePermission: Mutable,
+		}},
+	},
+	"[]interface{}": &ArraySlicePermission{
+		BasePermission: Mutable,
+		ElementPermission: &InterfacePermission{
+			BasePermission: Mutable,
+		},
+	},
+	"[5]interface{}": &ArraySlicePermission{
+		BasePermission: Mutable,
+		ElementPermission: &InterfacePermission{
+			BasePermission: Mutable,
+		},
+	},
+	"chan interface{}": &ChanPermission{
+		BasePermission: Mutable,
+		ElementPermission: &InterfacePermission{
+			BasePermission: Mutable,
+		},
+	},
+	"*interface{}": &PointerPermission{
+		BasePermission: Mutable,
+		Target: &InterfacePermission{
+			BasePermission: Mutable,
+		},
+	},
+	"map[interface{}] int": &MapPermission{
+		BasePermission: Mutable,
+		KeyPermission: &InterfacePermission{
+			BasePermission: Mutable,
+		},
+		ValuePermission: Mutable,
+	},
+	"func()": &FuncPermission{
+		BasePermission: Mutable,
+	},
+	"func(b interface{})": &FuncPermission{
+		BasePermission: Mutable,
+		Params: []Permission{
+			&InterfacePermission{
+				BasePermission: Mutable,
+			},
+		},
+	},
+	"func(b interface{})(c int)": &FuncPermission{
+		BasePermission: Mutable,
+		Params: []Permission{
+			&InterfacePermission{
+				BasePermission: Mutable,
+			},
+		},
+		Results: []Permission{Mutable},
+	},
+	"interface { foo(int) t}": newInterfaceWithMethod(),
+}
+
+func Parsed(s string) (types.Type, error) {
+	config := types.Config{}
+	fset := token.NewFileSet()
+	f, err := goparser.ParseFile(fset, s+".go", "package test\ntype t "+s, goparser.ParseComments)
+	if err != nil {
+		return nil, err
+	}
+	info := types.Info{Defs: make(map[*ast.Ident]types.Object)}
+	_, err = config.Check("hello", fset, []*ast.File{f}, &info)
+	if err != nil {
+		return nil, err
+	}
+
+	for ident, typ := range info.Defs {
+		if ident.Name == "t" {
+			return typ.Type(), nil
+		}
+	}
+	return nil, nil
+}
+
+func TestNewFromType(t *testing.T) {
+	for input, expected := range testCases {
+		input := input
+		expected := expected
+		t.Run(input, func(t *testing.T) {
+			typ, err := Parsed(input)
+			if err != nil {
+				t.Errorf("Invalid test input: %s", err)
+				return
+			}
+			if typ == nil {
+				t.Errorf("Does not parse to a type")
+				return
+			}
+			perm := NewTypeMapper().NewFromType(typ)
+			if !reflect.DeepEqual(perm, expected) {
+				t.Errorf("Input %s: Unexpected permission %#v, expected %#v - error: %v", input, perm, expected, err)
+			}
+		})
+	}
+}
