8055192df54e1b2fd7e579debb472d1521ccdc58
Julian Andres Klode <jak@jak-linux.org>
2017-09-14 14:44:09 +0200

    Add negative test case

--- capabilities/expr_test.go
+++ capabilities/expr_test.go
@@ -4,14 +4,16 @@
 package capabilities
 
 import (
+	"fmt"
 	"go/ast"
 	"go/parser"
+	"strings"
 	"testing"
 
 	"github.com/julian-klode/lingolang/permission"
 )
 
-func TestExpr(t *testing.T) {
+func TestVisitBinaryExpr(t *testing.T) {
 	st := NewStore()
 	i := &Interpreter{}
 
@@ -30,4 +32,21 @@ func TestExpr(t *testing.T) {
 	if !store.Equal(st) {
 		t.Errorf("Expected no change to store but was %v and is %v", st, store)
 	}
+
+}
+
+func TestVisitBinaryExpr_notreadable(t *testing.T) {
+	defer func() {
+		if e := recover(); e == nil || !strings.Contains(fmt.Sprint(e), "In b: Required permissions r, but only have w") {
+			t.Fatalf("Unexpected error %s", e)
+		}
+	}()
+	st := NewStore()
+	i := &Interpreter{}
+
+	e, _ := parser.ParseExpr("a + b")
+	st = st.Define(e.(*ast.BinaryExpr).X.(*ast.Ident), permission.Read)
+	st = st.Define(e.(*ast.BinaryExpr).Y.(*ast.Ident), permission.Write)
+
+	i.VisitExpr(st, e)
 }
