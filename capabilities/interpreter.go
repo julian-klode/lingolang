ef50019d73aee10f27b2ad3f623aea5e7b520a62
Julian Andres Klode <jak@jak-linux.org>
2017-09-18 12:52:57 +0200

    Initial abstract expression evaluator
    This is an initial version of abstractly interpreting
    expressions. It does not yet handle several things:
    
    * Functions with multiple results
    * Type assertions (value.(Type))
    * Composite literals (Type { values })
    * Selectors (a.b)
    * Function literals (we really need to store captured vars somewhere)
    * Short-circuiting semantics

---
+++ capabilities/interpreter.go
@@ -0,0 +1,243 @@
+// (C) 2017 Julian Andres Klode <jak@jak-linux.org>
+// Licensed under the 2-Clause BSD license, see LICENSE for more information.
+
+package capabilities
+
+import (
+	"fmt"
+	"go/ast"
+	"go/token"
+
+	"github.com/julian-klode/lingolang/permission"
+)
+
+// Interpreter interprets a given statement or expression.
+type Interpreter struct {
+}
+
+// Borrowed describes a variable that had to be borrowed from, along
+// with it's associated original permission.
+type Borrowed struct {
+	id   *ast.Ident
+	perm permission.Permission
+}
+
+// VisitExpr abstractly interprets permission changes by the expression.
+//
+//
+func (i *Interpreter) VisitExpr(st Store, e ast.Expr) (permission.Permission, []Borrowed, Store) {
+	if e == nil {
+		return permission.None, nil, st
+	}
+	switch e := e.(type) {
+	case *ast.BasicLit:
+		return i.visitBasicLit(st, e)
+	case *ast.BinaryExpr:
+		return i.visitBinaryExpr(st, e)
+	case *ast.CallExpr:
+		return i.visitCallExpr(st, e)
+	case *ast.CompositeLit:
+		return i.Error(e, "composite literal not yet implemented")
+	case *ast.FuncLit:
+		return i.Error(e, "function literals not yet implemented")
+	case *ast.Ident:
+		return i.visitIdent(st, e)
+		//return st.Assign(e, to)
+	case *ast.IndexExpr:
+		return i.visitIndexExpr(st, e)
+	case *ast.ParenExpr:
+		return i.VisitExpr(st, e.X)
+	case *ast.SelectorExpr:
+		return i.Error(e, "selector expressions not yet implemented")
+	case *ast.SliceExpr:
+		return i.visitSliceExpr(st, e)
+	case *ast.StarExpr:
+		return i.visitStarExpr(st, e)
+	case *ast.TypeAssertExpr:
+		return i.Error(e, "type assertion not yet implemented")
+	case *ast.UnaryExpr:
+		return i.visitUnaryExpr(st, e)
+	}
+	return i.Error(e, "Reached a bad expression %v - this should not happen", e)
+}
+
+// Release Releases all borrowed objects, and restores their previous permissions.
+func (i *Interpreter) Release(node ast.Node, st Store, undo []Borrowed) Store {
+	var err error
+	for _, b := range undo {
+		st, err = st.SetEffective(b.id, b.perm)
+		if err != nil {
+			i.Error(node, "Cannot release borrowed variable %s: %s", b.id, err)
+		}
+	}
+	return st
+}
+
+// Assert asserts that the base permissions of subject are a superset or the same as has.
+func (i *Interpreter) Error(node ast.Node, format string, args ...interface{}) (permission.Permission, []Borrowed, Store) {
+	panic(fmt.Errorf("%v: In %s: %s", node.Pos(), node, fmt.Sprintf(format, args...)))
+}
+
+func (i *Interpreter) Assert(node ast.Node, subject permission.Permission, has permission.BasePermission) {
+	if has&^subject.GetBasePermission() != 0 {
+		i.Error(node, "Required permissions %s, but only have %s", has, subject)
+	}
+}
+
+func (i *Interpreter) visitIdent(st Store, e *ast.Ident) (permission.Permission, []Borrowed, Store) {
+	perm := st.GetEffective(e)
+	if perm == nil {
+		i.Error(e, "Cannot borow %s: Unknown variable", e)
+	}
+	borrowed := []Borrowed{{e, perm}}
+	dead := permission.ConvertToBase(perm, permission.None)
+	st, err := st.SetEffective(e, dead)
+	if err != nil {
+		i.Error(e, "Cannot borrow identifier: %s", err)
+	}
+	return perm, borrowed, st
+}
+
+// visitBinaryExpr - A binary expression is either logical, arithmetic, or a comparison.
+func (i *Interpreter) visitBinaryExpr(st Store, e *ast.BinaryExpr) (permission.Permission, []Borrowed, Store) {
+	lhs, ldeps, st := i.VisitExpr(st, e.X)
+	rhs, rdeps, st := i.VisitExpr(st, e.Y)
+
+	// Requires: LHS, RHS readable
+	i.Assert(e.X, lhs, permission.Read)
+	i.Assert(e.Y, rhs, permission.Read)
+	// Ensures: Undo = nil
+	st = i.Release(e, st, ldeps)
+	st = i.Release(e, st, rdeps)
+	return permission.Owned | permission.Mutable, nil, st
+}
+
+// An index expression has the form A[B] and needs read permissions for both A and
+// B. A and B will be borrowed as needed. If used in a getting-way, we we could always
+// treat B as unowned, but in A[B] = C, B might need to be moved into A, therefore both
+// A and B will be dependencies of the result, at least for maps.
+func (i *Interpreter) visitIndexExpr(st Store, e *ast.IndexExpr) (permission.Permission, []Borrowed, Store) {
+	p1, deps1, st := i.VisitExpr(st, e.X)
+	p2, deps2, st := i.VisitExpr(st, e.Index)
+
+	// Requires: LHS and RHS are readable.
+	i.Assert(e.X, p1, permission.Read)
+	i.Assert(e.Index, p2, permission.Read)
+
+	switch p1 := p1.(type) {
+	case *permission.ArrayPermission:
+		// Ensures(array): Can only have integers here, no need to keep on to deps2
+		st = i.Release(e, st, deps2)
+		return p1.ElementPermission, deps1, st
+	case *permission.SlicePermission:
+		// Ensures(slice): Can only have integers here, no need to keep on to deps2
+		st = i.Release(e, st, deps2)
+		return p1.ElementPermission, deps1, st
+	case *permission.MapPermission:
+		// Ensures(map): If the key can be copied, we don't borrow it.
+		copy := permission.CopyableTo(p2, p1.KeyPermission)
+		if copy {
+			st = i.Release(e, st, deps2)
+		}
+
+		if copy || permission.MovableTo(p2, p1.KeyPermission) {
+			return p1.ValuePermission, deps1, st
+		}
+
+		i.Error(e, "Cannot move or copy from %s to %s", p2, p1.KeyPermission)
+	}
+
+	i.Error(e, "Indexing unknown type")
+	return nil, nil, nil
+}
+
+func (i *Interpreter) visitStarExpr(st Store, e *ast.StarExpr) (permission.Permission, []Borrowed, Store) {
+	p1, deps1, st := i.VisitExpr(st, e.X)
+	i.Assert(e.X, p1, permission.Read)
+
+	switch p1 := p1.(type) {
+	case *permission.PointerPermission:
+		return p1.Target, deps1, st
+	}
+
+	return i.Error(e, "Trying to dereference non-pointer %v", p1)
+}
+func (i *Interpreter) visitUnaryExpr(st Store, e *ast.UnaryExpr) (permission.Permission, []Borrowed, Store) {
+	p1, deps1, st := i.VisitExpr(st, e.X)
+	i.Assert(e.X, p1, permission.Read)
+
+	switch e.Op {
+	case token.AND:
+		return &permission.PointerPermission{
+			BasePermission: permission.Owned | permission.Mutable,
+			Target:         p1}, deps1, st
+	default:
+		st = i.Release(e, st, deps1)
+		return permission.Owned | permission.Mutable, nil, st
+	}
+}
+
+func (i *Interpreter) visitBasicLit(st Store, e *ast.BasicLit) (permission.Permission, []Borrowed, Store) {
+	return permission.Owned | permission.Mutable, nil, st
+}
+
+func (i *Interpreter) visitCallExpr(st Store, e *ast.CallExpr) (permission.Permission, []Borrowed, Store) {
+	fun, funDeps, st := i.VisitExpr(st, e.Fun)
+
+	switch fun := fun.(type) {
+	case *permission.FuncPermission:
+		for j, arg := range e.Args {
+			argPerm, argDeps, store := i.VisitExpr(st, arg)
+			st = store
+
+			if permission.CopyableTo(argPerm, fun.Params[j]) {
+				st = i.Release(e, st, argDeps)
+			} else if !permission.MovableTo(argPerm, fun.Params[j]) {
+				return i.Error(arg, "Cannot copy or move to parameter: Needed %#v, received %#v", fun.Params[j], argPerm)
+			}
+
+		}
+
+		// Call is done, release function permissions
+		i.Release(e, st, funDeps)
+
+		// FIXME: Multiple function results missing :(
+		if len(fun.Results) > 0 {
+			return fun.Results[0], nil, st
+		} else {
+			return permission.None, nil, st
+		}
+	default:
+		return i.Error(e, "Cannot call non-function object %v", fun)
+	}
+
+}
+
+func (i *Interpreter) visitSliceExpr(st Store, e *ast.SliceExpr) (permission.Permission, []Borrowed, Store) {
+	arr, arrDeps, st := i.VisitExpr(st, e.X)
+	low, lowDeps, st := i.VisitExpr(st, e.Low)
+	high, highDeps, st := i.VisitExpr(st, e.High)
+	max, maxDeps, st := i.VisitExpr(st, e.Max)
+
+	if e.Low != nil {
+		i.Assert(e.Low, low, permission.Read)
+	}
+	if e.High != nil {
+		i.Assert(e.High, high, permission.Read)
+	}
+	if e.Max != nil {
+		i.Assert(e.Max, max, permission.Read)
+	}
+
+	st = i.Release(e, st, maxDeps)
+	st = i.Release(e, st, highDeps)
+	st = i.Release(e, st, lowDeps)
+
+	switch arr := arr.(type) {
+	case *permission.ArrayPermission:
+		return &permission.SlicePermission{BasePermission: permission.Owned | permission.Mutable, ElementPermission: arr.ElementPermission}, arrDeps, st
+	case *permission.SlicePermission:
+		return arr, arrDeps, st
+	}
+	return i.Error(e, "Cannot create slice of %v - not sliceable", arr)
+}
---
+++ capabilities/interpreter_test.go
@@ -0,0 +1,206 @@
+// (C) 2017 Julian Andres Klode <jak@jak-linux.org>
+// Licensed under the 2-Clause BSD license, see LICENSE for more information.
+
+package capabilities
+
+import (
+	"fmt"
+	"go/ast"
+	"go/parser"
+	"reflect"
+	"strings"
+	"testing"
+
+	"github.com/julian-klode/lingolang/permission"
+)
+
+func recoverErrorOrFail(t *testing.T, message string) {
+	if e := recover(); e == nil || !strings.Contains(fmt.Sprint(e), message) {
+		t.Fatalf("Unexpected error -- %s -- expected it to contain -- %s --", e, message)
+	}
+}
+
+func runFuncRecover(t *testing.T, message string, block func()) {
+	defer recoverErrorOrFail(t, message)
+	block()
+}
+
+func newPermission(input interface{}) permission.Permission {
+	switch input := input.(type) {
+	case string:
+		perm, err := permission.NewParser(input).Parse()
+		if err != nil {
+			panic(err)
+		}
+		return perm
+	case permission.Permission:
+		return input
+	}
+	panic("Not reachable")
+}
+
+func TestVisitIdent(t *testing.T) {
+	st := Store{
+		{ast.NewIdent("x"), newPermission("om[]om"), newPermission("om")},
+	}
+	i := &Interpreter{}
+	runFuncRecover(t, "Unknown variable", func() {
+		i.VisitExpr(st, ast.NewIdent("a"))
+	})
+	runFuncRecover(t, "Cannot restrict effective permission", func() {
+		i.VisitExpr(st, st[0].ident)
+	})
+}
+
+func TestVisitExpr(t *testing.T) {
+	type errorResult string
+
+	testCases := []struct {
+		expr         string
+		name         string
+		lhs          interface{}
+		rhs          interface{}
+		result       interface{}
+		dependencies []string
+		lhsAfter     interface{}
+		rhsAfter     interface{}
+	}{
+		// ------------------- Binary expressions ----------------------------
+		{"a+b", "binaryOk", "or", "or", "om", []string{}, "or", "or"},
+		{"a+b", "binaryLhsUnreadable", "ow", "or", errorResult("In a: Required permissions r, but only have ow"), []string{}, "or", "or"},
+		{"a+b", "binaryLhsUnreadable", "or", "ow", errorResult("In b: Required permissions r, but only have ow"), []string{}, "or", "or"},
+		// ------------------- Indexing ----------------------------
+		{"a[b]", "mutableSlice", "om[]om", "om", "om", []string{"a"}, "n[]n", "om"},
+		{"a[b]", "mutableArray", "om[_]om", "om", "om", []string{"a"}, "n[_]n", "om"},
+		{"a[b]", "mutableMap", "om map[om]om", "om", "om", []string{"a"}, "n map[n]n", "om"},
+		// mutable map, non-copyable key: Item was moved into the map, it's gone now.
+		{"a[b]", "mutableMapNoCopyKey", "om map[om * om]om", "om * om", "om", []string{"a"}, "n map[n * r]n", "n * r"},
+		// a regular writable pointer is copyable. but beware: that's unsafe.
+		{"a[b]", "mutableMapCopyablePointerKey", "om map[orw * orw]om", "orw * orw", "om", []string{"a"}, "n map[n * rw]n", "orw * orw"},
+		// we pass a mutable key where we only need r/o, the key is consumed.
+		{"a[b]", "mutableMapFreeze", "om map[or * or]om", "om * om", "om", []string{"a"}, "n map[n * r]n", "n * r"},
+		{"a[b]", "notIndexable", "or", "ov", errorResult("Indexing unknown"), nil, "", ""},
+		{"a[b]", "keyNotReadable", "om[] om", "ow", errorResult("Required permission"), nil, "", ""},
+		{"a[b]", "indexableNotReadable", "on[] on", "or", errorResult("Required permission"), nil, "", ""},
+		{"a[b]", "mutableMapInvalidKey", "or map[om * om]ov", "ov * ov", errorResult("move or copy"), nil, "", ""},
+		// ------------------- Star expressions ----------------------------
+		{"*b", "mutablePointer", "", "om * om", "om", []string{"b"}, "", "n * r"},
+		{"*b", "mutablePointerReadTarget", "", "om * or", "or", []string{"b"}, "", "n * r"},
+		{"*b", "readOnlyPointer", "", "or * or", "or", []string{"b"}, "", "n * r"},
+		{"*b", "noPointer", "", "or", errorResult("non-pointer"), []string{"b"}, "", "n * r"},
+		// Unary expressions
+		{"-b", "mutableNegation", nil, "om", "om", []string{}, nil, "om"},
+		{"&b", "mutableNegation", nil, "om", "om * om", []string{"b"}, nil, "n"},
+		{"&b", "mutableNegation", nil, "or", "om * or", []string{"b"}, nil, "n"},
+		// Parens
+		{"(b)", "parenMut", nil, "om", "om", []string{"b"}, nil, "n"},
+		{"(b)", "parenPoint", nil, "om * om", "om * om", []string{"b"}, nil, "n * r"},
+		// Function calls
+		{"a(b)", "callMutableNoCopy", "om func(om * om) or", "om * om", "or", []string{}, "om func(om * om) or", "n * r"},
+		{"a(b)", "callMutableCopy", "om func(om) or", "om", "or", []string{}, "om func(om) or", "om"},
+		{"a(b)", "callMutableNoRet", "om func(om)", "om", "n", []string{}, "om func(om)", "om"},
+		{"a(b)", "callMutableIncompat", "om func(om * om)", "or * or", errorResult("not copy or move"), []string{}, "om func(om)", "or * or"},
+
+		{"a(b)", "callRetValue", "om func(om) n", "om", "n", []string{}, "om func(om) n", "om"},
+		{"a(b)", "callNotAFunction", "om", "om", errorResult("non-function"), []string{}, "om", "om"},
+		// Basic lit
+		{"127", "basicLitInt", nil, nil, "om", []string{}, nil, nil},
+		{"127.1", "basicLitFloat", nil, nil, "om", []string{}, nil, nil},
+		{"0i", "basicLitImag", nil, nil, "om", []string{}, nil, nil},
+		{"'c'", "basicLitChar", nil, nil, "om", []string{}, nil, nil},
+		{"\"string\"", "basicLitString", nil, nil, "om", []string{}, nil, nil},
+		// Slice
+		{"a[:]", "sliceAllArr", "om [_]ov", nil, "om []ov", []string{"a"}, "n [_]n", nil},
+		{"a[:]", "sliceAllSlice", "om []ov", nil, "om []ov", []string{"a"}, "n []n", nil},
+		{"a[:]", "sliceAllSliceRo", "or []or", nil, "or []or", []string{"a"}, "n []n", nil},
+		{"a[:b]", "sliceHigh", "om []ov", "om", "om []ov", []string{"a"}, "n []n", "om"},
+		{"a[b:2:3]", "sliceMin", "om []ov", "om", "om []ov", []string{"a"}, "n []n", "om"},
+		{"a[1:2:b]", "sliceMax", "om []ov", "om", "om []ov", []string{"a"}, "n []n", "om"},
+		{"a[1:2:b]", "sliceInvalid", "om map[ov]ov", "om", errorResult("not sliceable"), []string{"a"}, "n []n", "om"},
+		// TODO
+		{"T {1}", "compositeLit", "om", "om", errorResult("not yet implemented"), nil, nil, nil},
+		{"func() {}", "funcLit", "om", "om", errorResult("not yet implemented"), nil, nil, nil},
+		{"a.b", "selector", "om", "om", errorResult("not yet implemented"), nil, nil, nil},
+		{"a.(b)", "selector", "om", "om", errorResult("not yet implemented"), nil, nil, nil},
+	}
+
+	for _, test := range testCases {
+
+		t.Run(test.name, func(t *testing.T) {
+			st := NewStore()
+			i := &Interpreter{}
+
+			var lhs *ast.Ident
+			var rhs *ast.Ident
+			e, _ := parser.ParseExpr(test.expr)
+			ast.Inspect(e, func(node ast.Node) bool {
+				if ident, ok := node.(*ast.Ident); ok {
+					switch ident.Name {
+					case "a":
+						lhs = ident
+					case "b":
+						rhs = ident
+					}
+				}
+				return true
+			})
+
+			if lhs != nil {
+				st = st.Define(lhs, newPermission(test.lhs))
+			}
+			if rhs != nil {
+				st = st.Define(rhs, newPermission(test.rhs))
+			}
+
+			if eResult, ok := test.result.(errorResult); ok {
+				runFuncRecover(t, string(eResult), func() {
+					i.VisitExpr(st, e)
+				})
+				return
+			}
+
+			perm, deps, store := i.VisitExpr(st, e)
+
+			if !reflect.DeepEqual(newPermission(test.result), perm) {
+				t.Errorf("Evaluated to %v, expected %v", perm, newPermission(test.result))
+			}
+
+			// Check dependencies
+			depsAsString := make([]string, len(deps))
+			for i := range deps {
+				depsAsString[i] = deps[i].id.Name
+			}
+
+			if !reflect.DeepEqual(depsAsString, test.dependencies) {
+				t.Errorf("Found dependencies %v, expected %v", depsAsString, test.dependencies)
+			}
+
+			if lhs != nil && !reflect.DeepEqual(store.GetEffective(lhs), newPermission(test.lhsAfter)) {
+				t.Errorf("Found lhs after = %v, expected %v", store.GetEffective(lhs), newPermission(test.lhsAfter))
+			}
+			if rhs != nil && !reflect.DeepEqual(store.GetEffective(rhs), newPermission(test.rhsAfter)) {
+				t.Errorf("Found rhs after = %v, expected %v", store.GetEffective(rhs), newPermission(test.rhsAfter))
+			}
+		})
+
+	}
+}
+
+func TestVisitExpr_bad(t *testing.T) {
+	var i Interpreter
+	var st Store
+	runFuncRecover(t, "bad expr", func() {
+		i.VisitExpr(st, &ast.BadExpr{})
+	})
+}
+
+func TestRelease(t *testing.T) {
+	var i Interpreter
+	var st Store
+	runFuncRecover(t, "not release borrowed variable", func() {
+		st = st.Define(ast.NewIdent("a"), newPermission("om"))
+		i.Release(st[0].ident, st, []Borrowed{
+			{st[0].ident, newPermission("om * om")},
+		})
+	})
+}
